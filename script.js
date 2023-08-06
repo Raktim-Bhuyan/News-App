f257002f754daa4d79499478bd607d80e9235337
Raktim-Bhuyan <rokuongo@gmail.com>
2023-08-06 11:16:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const API_KEY = "55d594bf7bf64e6bb2d36d5e02864ad5";
+// q means query
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load", () => fetchNews("India"));
+
+async function fetchNews(query) {
+  const response = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+  const data = await response.json();
+  bindData(data.articles);
+  console.log(data);
+}
+function bindData(articles) {
+  const cardsContainer = document.getElementById("cards-container");
+  const newsCardTemplate = document.getElementById("news-card-template");
+
+  cardsContainer.innerHTML = "";
+  articles.forEach((article) => {
+    if (!article.urlToImage) return;
+    const cardClone = newsCardTemplate.content.cloneNode(true);
+    // creates a deep copy
+    fillDataInCard(cardClone, article);
+    cardsContainer.appendChild(cardClone);
+  });
+}
+function fillDataInCard(cardClone, article) {
+  const newsImg = cardClone.querySelector("#news-img");
+  const newsTitle = cardClone.querySelector("#news-title");
+  const newsSource = cardClone.querySelector("#news-source");
+  const newsDescription = cardClone.querySelector("#news-desc");
+
+  newsImg.src = article.urlToImage;
+  newsTitle.innerHTML = article.title;
+  //   newsSource.innerHTML = article.;
+  newsDescription.innerHTML = article.description;
+  const date = new Date(article.publishedAt).toLocaleString("en-US", {
+    timeZone: "Asia/Jakarta",
+  });
+  newsSource.innerHTML = `${article.source.name} ${date}`;
+  cardClone.firstElementChild.addEventListener("click", () => {
+    window.open(article.url, "_blank"); //blank will take you to new tab on clicking the firstElementChild
+    //i.e the card div
+  });
+}
+let currSelectedNav = null;
+function onNavItemClick(id) {
+  fetchNews(id);
+  const navItem = document.getElementById(id);
+  currSelectedNav?.classList.remove("active"); //currSelectedNav is not equal to null then remove the
+  //active class from currSelectedNav
+  currSelectedNav = navItem;
+  currSelectedNav.classList.add("active");
+}
